0265bcd6f65dc345824391bfaf0dca039f0c666a
razeek-j <moosarazeek@gmail.com>
2024-07-16 20:16:42 +0530

    Cleared the sidebar

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,12 @@
+import SideBar from "@/components/SideBar";
+
 export default function Dashboard() {
   return (
-    <div>
-      HEllo
+    <div className="bg-grey h-screen flex flex-row justify-start">
+      <SideBar />
+      <div className="flex-1 p-4 border border-dashed">
+        Gemini
+      </div>
     </div>
   )
 }
---
+++ src/components/SideBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SideBar = () => {
+  return (
+    <div className='h-screen font-poppins px-4 pt-4 pb-4 flex justify-between flex-col w-72 bg-stone-100'>
+        SideBar
+    </div>
+  )
+}
+
+export default SideBar
