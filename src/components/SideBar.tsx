ca6d3da150ddcb4784dca8e85b873e4880be3014
razeek-j <moosarazeek@gmail.com>
2024-07-18 21:28:31 +0530

    Final

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useState } from 'react';
 import classNames from 'classnames';
-import { Bars3Icon as MenuIcon, XMarkIcon as XIcon, ChatBubbleLeftEllipsisIcon, PlusIcon } from '@heroicons/react/24/outline';
+import { Bars3Icon as MenuIcon, XMarkIcon as XIcon, ChatBubbleLeftEllipsisIcon, PlusIcon, CogIcon, QuestionMarkCircleIcon, ClockIcon } from '@heroicons/react/24/outline';
 
 const SideBar = () => {
   const [toggleCollapse, setToggleCollapse] = useState(false);
@@ -13,6 +13,8 @@ const SideBar = () => {
     }
   );
 
+  const buttonClasses = 'p-2 w-full flex items-center gap-2 hover:bg-gray-200 rounded-lg';
+
   return (
     <div className={wrapperClasses}>
       <div className="flex flex-col h-full">
@@ -21,26 +23,42 @@ const SideBar = () => {
             {!toggleCollapse ? <XIcon className="w-6 h-6" /> : <MenuIcon className="w-6 h-6" />}
           </button>
         </div>
-        {!toggleCollapse && (
-          <nav className="flex-1 flex flex-col items-start justify-center mt-4">
-            <button className="bg-gray-200 rounded-full p-2 flex items-center gap-2 mb-5">
-                <PlusIcon className="w-5 h-5" />
-                <span className="text-sm text-gray-600">New Chat</span>
-              </button>
-            <h2 className="font-bold mb-4 text-gray-600">Recents</h2>
-            <ul className="text-left">
-              <li className="py-2 px-4 hover:bg-gray-200 rounded flex items-center gap-2">
-                <ChatBubbleLeftEllipsisIcon className="w-5 h-5" /> what is next.js
-              </li>
-              <li className="py-2 px-4 hover:bg-gray-200 rounded flex items-center gap-2">
-                <ChatBubbleLeftEllipsisIcon className="w-5 h-5" /> How to code
-              </li>
-              <li className="py-2 px-4 hover:bg-gray-200 rounded flex items-center gap-2">
-                <ChatBubbleLeftEllipsisIcon className="w-5 h-5" /> what is a computer
-              </li>
-            </ul>
-          </nav>
-        )}
+        <nav className="flex-1 flex flex-col items-start justify-center mt-4">
+          <button className="bg-gray-300 p-2 flex items-center gap-2 mb-5 rounded-full">
+            <PlusIcon className="w-5 h-5 text-gray-600" />
+            {!toggleCollapse && <span className="text-sm text-gray-600">New Chat</span>}
+          </button>
+          {!toggleCollapse && (
+            <>
+              <h2 className="font-bold mb-4 text-gray-600">Recents</h2>
+              <ul className="text-left w-full">
+                <li className={buttonClasses}>
+                  <ChatBubbleLeftEllipsisIcon className="w-5 h-5" /> what is next.js
+                </li>
+                <li className={buttonClasses}>
+                  <ChatBubbleLeftEllipsisIcon className="w-5 h-5" /> How to code
+                </li>
+                <li className={buttonClasses}>
+                  <ChatBubbleLeftEllipsisIcon className="w-5 h-5" /> what is a computer
+                </li>
+              </ul>
+            </>
+          )}
+        </nav>
+        <div className="mt-auto flex flex-col items-start justify-center mb-4 w-full">
+          <button className={buttonClasses}>
+            <ClockIcon className="w-5 h-5" />
+            {!toggleCollapse && <span className="text-sm text-gray-600">Activities</span>}
+          </button>
+          <button className={buttonClasses}>
+            <QuestionMarkCircleIcon className="w-5 h-5" />
+            {!toggleCollapse && <span className="text-sm text-gray-600">Help</span>}
+          </button>
+          <button className={buttonClasses}>
+            <CogIcon className="w-5 h-5" />
+            {!toggleCollapse && <span className="text-sm text-gray-600">Settings</span>}
+          </button>
+        </div>
       </div>
     </div>
   );
